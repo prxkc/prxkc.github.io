a98c1e25857cee46f0c5f15cfc67ab9decf0bef1
Shakil Islam <islamshakil17@gmail.com>
2024-04-17 04:06:40 +0600

    Redo Everything

--- script.js
+++
@@ -1,7 +0,0 @@
-const gameStart = document.getElementById('gameStart');
-const descriptionBox = document.querySelector('.description-box');
-
-gameStart.addEventListener('click', function() {
-    gameStart.style.display = 'none';
-    descriptionBox.classList.add('expanded');
-});
